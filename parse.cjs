43a81ced0ef212d10766339849ada0d3e5976af4
Anton Piliugin <anton.piliugin@icloud.com>
2023-12-02 16:48:11 +0500

    Pagination

--- parse.cjs
+++ parse.cjs
@@ -5,31 +5,57 @@ const puppeteer = require('puppeteer-extra')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
-puppeteer.launch({ headless: 'new' }).then(async (browser) => {
+const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+puppeteer.launch({ headless: false/*'new'*/ }).then(async (browser) => {
   const page = await browser.newPage()
-  await page.goto('https://www.dns-shop.ru/catalog/17a8d26216404e77/vstraivaemye-xolodilniki/')
-  const element = await page.waitForSelector('[data-catalog-products=""] div.product-buy__price')
-  const info = await page.evaluate(() => {
-    const elements = [...document.querySelectorAll('a.catalog-product__name, [data-catalog-products=""] div.product-buy__price')]
+  await page.goto('https://www.dns-shop.ru/catalog/17a8d26216404e77/vstraivaemye-xolodilniki/', {
+    waitUntil: 'networkidle2'
+  })
 
-    const result = []
+  const paginationSelector = 'ul.pagination-widget__pages'
+  const pagination = await page.waitForSelector(paginationSelector, { visible: true })
 
-    for (let index = 0; index < elements.length; index+=2) {
+  const pages = await pagination.evaluate((page) => page.textContent)
+  const totalPages = pages && pages.length > 1 ? parseInt(pages.substring(pages.length - 1), 10) : 1
+
+  const buttonSelector = 'button.pagination-widget__show-more-btn'
+
+  for (let currentPage = 1; currentPage < totalPages; currentPage++) {
+    await page.waitForSelector(buttonSelector, { visible: true })
+    await page.click(buttonSelector)
+    await page.waitForSelector('div.catalog-preloader_active', { hidden: false })
+    await page.waitForSelector('div.catalog-preloader_active', { hidden: true })
+    await page.waitForTimeout(randomIntFromInterval(2134, 3456))
+  }
+
+  const result = await page.evaluate(() => {
+    const items = []
+
+    const elements = [...document.querySelectorAll('a.catalog-product__name, div.product-buy__price')]
+
+    if (!elements.length) return items
+
+    for (let index = 0; index < elements.length; index += 2) {
       const text = elements[index].textContent
-      const price = elements[index + 1].childNodes[0].textContent
+      const priceElement = elements[index + 1]
+      const price = priceElement?.childNodes[0]?.textContent || priceElement.textContent
 
-      result.push(`"${text}"|${price}`)
+      items.push(`"${text}"|${price}`)
     }
 
-    return result
+    return items
   })
   
-  const header = 'Наименование|Цена'
-  info.unshift(header)
+  if (result.length) {
+    const header = 'Наименование|Цена'
+    result.unshift(header)
 
-  const CSV = info.join('\n')
-  fs.writeFileSync('result.csv', CSV)
+    const CSV = result.join('\n')
+    fs.writeFileSync('result.csv', CSV)
+  } else {
+    console.log('Не найдено товаров')
+  }
 
-  await element.dispose()
   await browser.close()
 })
